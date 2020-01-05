28937a18148c870c2f4a5a36bbb2e1e1f7bd4a61
Tim Simpson <timsimpson4@gmail.com>
2020-01-05 02:22:17 -0600

    fix other appveyor batch files

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -1,7 +1,11 @@
+if "%APPVEYOR_BUILD_FOLDER%"=="" (
+    echo This is expected to be run from AppVeyor.
+    exit /b 1
+)
+
 set PYTHON="C:\Python37"
 set PATH=%PYTHON%\Scripts;%PATH%
 
-pip.exe install conan --upgrade
-
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-conan user
+call pip.exe install conan --upgrade
+call conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+call conan user
--- ci/appveyor/run.bat
+++ ci/appveyor/run.bat
@@ -1,35 +1,9 @@
-REM - This script assumes it's run from the root directory of the project.
+if "%APPVEYOR_BUILD_FOLDER%"=="" (
+    echo This is expected to be run from AppVeyor.
+    exit /b 1
+)
 
-SET root_dir=%CD%
+set PYTHON="C:\Python37"
+set PATH=%PYTHON%\Scripts;%PATH%
 
-SET PATH=%PATH%;%PYTHON%\Scripts\
-SET LP3_ROOT_PATH=%root_dir%\ci\media
-
-REM - TODO: Test other configurations
-
-md output
-md output/windows
-
-SET build_dir="output/windows"
-cd %build_dir%
-
-echo 'Oh Windows, how I once loved thee. But behind that mask of lies'
-echo ''
-echo '  O O_-    A spider'
-echo ' //;; \\\\   whom wilst strick'
-echo '
-
-echo "What version of Python is this?"
-python --version
-
-mkdir output
-cd output
-
-conan install %root_dir% -pr=..\ci\profiles/msvc --build missing
-
-echo "What is in current directory?"
-dir
-
-cat "conan_paths.cmake"
-
-conan build %root_dir%
+%~dp0\..\run.bat %*
