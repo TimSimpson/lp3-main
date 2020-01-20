79f9f55b1f9adfab72308590a17b4bceef9c2d64
Tim Simpson <timsimpson4@gmail.com>
2020-01-19 20:33:15 -0600

    try to build docs

---
+++ ci/travis/docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "${TRAVIS_BUILD_DIR}" == '' ]; then
+    echo "This is intended too run on Travis CI."
+    exit 1
+fi
+set -e
+set -x
+
+readonly scripts_dir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+
+export PATH="$(pwd)/venv/bin:${PATH}"
+
+pip install mrst = "^0.5.2"
+pip install Sphinx = "*"
+mrst --source docs --output output/doc
